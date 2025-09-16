f86275e620058c8a05fb77d23c2f14b81065a916
Bahubali Nalte <142397022+BahubaliNalte@users.noreply.github.com>
2025-09-16 22:27:57 +0530

    profile page error

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Firebase config (do not commit this file)
+const firebaseConfig = {
+  apiKey: "AIzaSyBPgF0eZpGKOt35or9KX7Yx0FqLh2DhZeg",
+  authDomain: "claryguide-2e0e6.firebaseapp.com",
+  databaseURL: "https://claryguide-2e0e6-default-rtdb.firebaseio.com/",
+  projectId: "claryguide-2e0e6",
+  storageBucket: "claryguide-2e0e6.firebasestorage.app",
+  messagingSenderId: "998554914908",
+  appId: "1:998554914908:web:e92af109ff65db87eb5699",
+  measurementId: "G-0BWDK4P3LE"
+};
