1f9185476773c418a609f3bd78570990bd291f9e
Bahubali Nalte <142397022+BahubaliNalte@users.noreply.github.com>
2025-09-16 13:05:40 +0530

    profile sec update

--- src/script.js
+++ src/script.js
@@ -8,17 +8,8 @@
    - simple swipe for features on mobile
 */
 
-// Firebase config 
-const firebaseConfig = {
-  apiKey: "AIzaSyBPgF0eZpGKOt35or9KX7Yx0FqLh2DhZeg",
-  authDomain: "claryguide-2e0e6.firebaseapp.com",
-  databaseURL: "https://claryguide-2e0e6-default-rtdb.firebaseio.com/",
-  projectId: "claryguide-2e0e6",
-  storageBucket: "claryguide-2e0e6.firebasestorage.app",
-  messagingSenderId: "998554914908",
-  appId: "1:998554914908:web:e92af109ff65db87eb5699",
-  measurementId: "G-0BWDK4P3LE"
-};
+// Import firebaseConfig from separate file
+// Make sure firebaseConfig.js is loaded before this script in HTML
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
